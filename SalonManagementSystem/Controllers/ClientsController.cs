bcb800c5e5d2fda326e5a436fe235e929a8c680c
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-23 20:09:54 -0700

    added controller and index view for client

--- SalonManagementSystem/Controllers/ClientsController.cs
+++ SalonManagementSystem/Controllers/ClientsController.cs
@@ -10,5 +10,17 @@ namespace SalonManagementSystem.Controllers
 {
     public class ClientsController : Controller
     {
-        
+        private readonly SalonManagementSystemContext _db;
+
+        public ClientsController(SalonManagementSystemContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<SClient> model = _db.Clients.ToList();
+            return View(model);
+        }
+
     }
--- SalonManagementSystem/Controllers/StylistsController.cs
+++ SalonManagementSystem/Controllers/StylistsController.cs
@@ -23,3 +23,4 @@ namespace SalonManagementSystem.Controllers
             return View(model);
         }
     }
+}
