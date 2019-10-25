dde42ec6558c8655a8ca0e78ce8288587c8470ca
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-24 21:44:20 -0700

    added delete function in the controller and clients views

--- SalonManagementSystem/Controllers/ClientsController.cs
+++ SalonManagementSystem/Controllers/ClientsController.cs
@@ -34,7 +34,7 @@ namespace SalonManagementSystem.Controllers
         {
             return View();
         }
-        
+
         [HttpPost]
         public ActionResult Create(Client client)
         {
@@ -47,6 +47,37 @@ namespace SalonManagementSystem.Controllers
         {
             Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
             return View(thisClient);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+
+            Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+
+            return View(thisClient);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            _db.Entry(client).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+            return View(thisClient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+            _db.Clients.Remove(thisClient);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
     }
