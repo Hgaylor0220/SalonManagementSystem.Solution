914699298505aa75ecd5d9fe164d76760965c498
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-27 21:34:15 -0700

    working on bugs with for and foreach in stylist/creat

--- SalonManagementSystem/Controllers/ClientsController.cs
+++ SalonManagementSystem/Controllers/ClientsController.cs
@@ -2,12 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SalonManagementSystem.Models;
 using SalonManagementSystem.Data;
